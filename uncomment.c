495cfbc06e3bfa6709f39b8c3be592fae3e46647
DaKnig <37626476+DaKnig@users.noreply.github.com>
2018-03-24 13:55:49 +0200

    Add files via upload

--- uncomment.c
+++ uncomment.c
@@ -3,36 +3,44 @@
 
 void cpstr(FILE* in,FILE* out,char chr){
 	fputc(chr,out);
-	int buff=fgetc(in);
-	while (buff != EOF && buff != chr && buff!='\n'){
-		fputc(buff,out);
-		buff=fgetc(in);
+	int buff_chr=fgetc(in);
+	while (buff_chr != EOF && buff_chr != chr && buff_chr!='\n'){
+		fputc(buff_chr,out);
+		buff_chr=fgetc(in);
 	}
-	fputc(buff,out);
+	fputc(buff_chr,out);
 }
 
 void skip_line(FILE* in){
-	int buff;
+	int buff_chr;
 	do
-		buff=fgetc(in);
-	while(buff != '\n' && buff != EOF);
-	ungetc(buff,in);
+		buff_chr=fgetc(in);
+	while(buff_chr != '\n' && buff_chr != EOF);
+	ungetc(buff_chr,in);
 }
 
 void skip_comment(FILE* in, FILE* out){
-	int buff[2];
-	buff[1]='E';
+	int buff_chr[2];
+	buff_chr[1]='E';
 
 	do{
-		buff[0]=buff[1];
-		buff[1]=fgetc(in);
-		if(buff[1]=='\n')	fputc('\n',out);
-	}while( !( (buff[1]==EOF) || ( (buff[0]=='*' && buff[1]=='/') ) ) );
+		buff_chr[0]=buff_chr[1];
+		buff_chr[1]=fgetc(in);
+		if(buff_chr[1]=='\n')	fputc('\n',out);
+	}while( !( (buff_chr[1]==EOF) || ( (buff_chr[0]=='*' && buff_chr[1]=='/') ) ) );
 }
 
 int  main(int argc,char** argv){
 
-	
+	FILE* in = fopen( (argc>1?argv[1]:"input") ,"r");
+	if (in==NULL){
+		perror("can't open input file for reading");
+		exit(1);	}
+	FILE* out = fopen( (argc>2?argv[2]:"output") ,"w");
+      if (out==NULL){
+      	perror("can't open output file for writing");
+            exit(1);	}
+
 	switch (argc){
 		case 1: printf("default names \"input\", \"output\" would be used\n");
 			break;
@@ -42,35 +50,27 @@ int  main(int argc,char** argv){
 			return 0;
 	}
 
-	FILE* in = fopen( (argc>1?argv[1]:"input") ,"r");
-	if (in==NULL){
-		perror("cant open input file for reading");
-		exit(1);	}
-	FILE* out = fopen((argc>2?argv[2]:"output"),"w");
-        if (out==NULL){
-                perror("cant open output file for writing");
-                exit(1);        }
+	int buff_chr;
+	buff_chr=fgetc(in);
 
-
-	int buff[2];
-	buff[0]='E',buff[1]=fgetc(in);
-
-	while(buff[1] != EOF){
-		switch(buff[1]){
-			case ';':	fputc(';',out);	skip_line(in);	break;
+	while(buff_chr != EOF){
+		switch(buff_chr){
+			case ';':
+				fputc(';',out);	skip_line(in);	break;
 			case '/':
-				buff[0]=fgetc(in);
-                        	switch (buff[0]){
-                                	case '/': skip_line(in);        break;
-                                	case '*': skip_comment(in,out); break;
-                                	default : fputc('/',out);       ungetc(buff[0],in);     break;
+				buff_chr=fgetc(in);
+                  	switch (buff_chr){
+                          	case '/': skip_line(in);        break;
+                          	case '*': skip_comment(in,out); break;
+                          	default : fputc('/',out);       ungetc(buff_chr,in);     break;
 				}
 				break;
 			case '\"': case '\'':
-					cpstr(in,out,buff[1]);	break;
-			default:	fputc(buff[1],out);	break;
+				cpstr(in,out,buff_chr);	break;
+			default:
+				fputc(buff_chr,out);	break;
 		}
-		buff[1]=fgetc(in);
+		buff_chr=fgetc(in);
 	}
 	fclose(in);fclose(out);
 	return 0;
